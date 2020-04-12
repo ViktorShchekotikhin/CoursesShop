d8f6f1e1c811206e2973aa0a5114c71617a38969
ViktorShchekotikhin <vitia46@gmail.com>
2020-04-12 18:26:04 +0300

    change keys

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,11 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('keys.prod')
+    module.exports = {
+        MONGODB_URI: process.env.MONGODB_URI,
+        SESSION_SECRET: process.env.SESSION_SECRET,
+        SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+        EMAIL_FROM: process.env.EMAIL_FROM,
+        BASE_URL: process.env.BASE_URL
+    };
 } else {
     module.exports = require('keys.dev')
 }
